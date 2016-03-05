c067fd0e2035c240cc8386506d15761348123ec8
JolinGom <ksjksjgg@gmail.com>
2016-03-06 06:58:24 +0900

    Initial Commit

---
+++ ch02/tests/test.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# 2D-list len()
+# x=[[1,2,3],
+#    [4,5,6]]
+# print(len(x)) # length of row
+# print(len(x[0])) # length of column
+# print(len(x[:,0])) # Error ???
+
+
+# Numpy copy()
+# >>> x = np.array([1, 2, 3])
+# >>> y = x #Reference Copy
+# >>> z = np.copy(x) # real copy
+# x =np.array([1, 2, 3])
+# z= x.copy()
+# print(z)
+# z[0]=4
+# print(z[0])
+# print(x[0])
+
+
+# python list copy()
+# x =[1, 2, 3]
+# z= x.copy()
+# print(z)
+# z[0]=4
+# print(z[0])
+# print(x[0])
+
+
+# Numpy indexing
+# y=np.array(['one','two','three'])
+# x = np.array([0, 1,1, 2, 2,2])
+# print(y[x])
+
+#Numpy ones()
+# print(np.ones(5,bool))
